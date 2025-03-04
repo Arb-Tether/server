f4a8f9ceca6f847130d0f4aed4a567e5a68201dc
sonjiseokk <thswltjr11@gmail.com>
2025-03-04 18:06:42 +0900

    feat: 메시징 DTO 구현

---
+++ price-collector/src/main/java/com/arbiter/pricecollector/model/dto/PriceMessageDto.java
@@ -0,0 +1,23 @@
+package com.arbiter.pricecollector.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceMessageDto {
+    @JsonProperty("trade_price") // JSON의 "trade_price"를 price 필드에 매핑
+    private Double price;
+
+    @JsonProperty("code") // JSON의 "code"를 ticker 필드에 매핑
+    private String ticker;
+
+    @JsonProperty("trade_timestamp") // JSON의 "trade_timestamp"를 timestamp 필드에 매핑
+    private Long timestamp;
+}
+
+
