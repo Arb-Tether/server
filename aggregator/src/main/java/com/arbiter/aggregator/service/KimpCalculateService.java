7ace442eb26da1592ec1ea8406c675787bffd1ef
sonjiseokk <thswltjr11@gmail.com>
2025-03-07 20:51:34 +0900

    feat: 김프 계산 서비스 추가

---
+++ aggregator/src/main/java/com/arbiter/aggregator/service/KimpCalculateService.java
@@ -0,0 +1,14 @@
+package com.arbiter.aggregator.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KimpCalculateService {
+    public double calculateKimpPrice(double upbit, double binance, double usdtkrw) {
+        return (upbit - (binance * usdtkrw)) / (binance * usdtkrw) * 100;
+    }
+}
