e584e638bc20beea27c8ec1ae11eb5fb9a79bea2
sonjiseokk <thswltjr11@gmail.com>
2025-03-07 20:53:12 +0900

    chore: price-collector 두번째 모듈 추가

---
+++ price-collector2/src/main/java/com/arbiter/pricecollector2/PriceCollector2Application.java
@@ -0,0 +1,13 @@
+package com.arbiter.pricecollector2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCollector2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceCollector2Application.class, args);
+    }
+
+}
---
+++ price-collector2/src/test/java/com/arbiter/pricecollector2/PriceCollector2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbiter.pricecollector2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceCollector2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
