e347e6328a1384e4cb6e98b42751cde9a1f1452c
sonjiseokk <thswltjr11@gmail.com>
2025-03-08 21:24:42 +0900

    feat: 카프카 관련 설정 추가 및 produce 로직 구현

---
+++ price-collector2/src/main/java/com/arbiter/pricecollector2/service/message/KafkaProducerService.java
@@ -0,0 +1,21 @@
+package com.arbiter.pricecollector2.service.message;
+
+import com.arbiter.core.model.PriceMessageDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, PriceMessageDto> kafkaTemplate;
+    private static final String TOPIC = "price_topic";
+
+    public void sendPrice(PriceMessageDto priceMessage) {
+        kafkaTemplate.send(TOPIC, priceMessage);
+        log.info("[Kafka] 메시지 전송: {}", priceMessage);
+    }
+}
